da068b40039ab0049d5ca9922f844e3bb11200d0
Matheus Santana <embs@cin.ufpe.br>
2018-07-11 18:02:35 -0300

    Plot results

---
+++ plot.r
@@ -0,0 +1,57 @@
+#!/usr/bin/env Rscript
+
+pdf(NULL) # Avoid creating Rplots.pdf
+
+library(ggplot2)
+
+#
+#  It would be so great to reuse this function (it is basically a copy & paste
+#  from the script for plotting the binsizes chart).
+#
+plot_func <- function(ylab, data, errs, filename) {
+  microservice=c(
+                 rep("carts",    2),
+                 rep("orders",   2),
+                 rep("payment",  2),
+                 rep("shipping", 2),
+                 rep("user",     2)
+                 )
+  languages=c(rep("Java",2),
+              rep("Java",2),
+              rep("Go Lang",2),
+              rep("Java",2),
+              rep("Go Lang",2))
+  condition=rep(c("Instrumented\nMicroservices", "Rbinder") , 5)
+  plotdata=data.frame(microservice,languages,condition,data,errs)
+ 
+  # Bar plot with error bars.
+  theme_set(theme_bw())
+  plot <- ggplot(plotdata, aes(x=microservice, y=data, fill=condition)) +
+                  geom_bar(stat="identity", position="dodge", colour="black") +
+                  geom_errorbar(aes(ymin=means-errs, ymax=means+errs),
+                                width=.2,
+                                position=position_dodge(.9))
+  plot + labs(x="Microservice", y=ylab) +
+    scale_fill_manual("Scenario",
+                      values = c("Instrumented Microservices"="white",
+                                 "Rbinder"="gray")
+                      ) +
+    facet_grid(~languages, scales='free', space='free')
+  ggsave(filename=paste('./', filename, '.pdf', sep=''), height=3)
+}
+
+means <- c()
+errs <- c()
+meansidx <- 1
+
+for(service in c('carts', 'orders', 'payment', 'shipping', 'user')) {
+  for(scenario in c('instrumented', 'rbinder')) {
+    filename <- paste('./logs/', scenario, '-', service, '-start-time', '.log', sep='')
+    replicaset <- read.csv(file=filename, head=FALSE)
+    means[meansidx] <- mean(replicaset$V1)
+    errs[meansidx] <- qt(0.975, df=length(replicaset$V1)-1)*sd(replicaset$V1)/sqrt(length(replicaset$V1))
+    meansidx <- meansidx + 1
+  }
+}
+
+plot_func("Time (s)", means, errs, 'time')
